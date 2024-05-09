4420f969333a4cb717d097400ac17028318e3ee6
fdtgyhcuj <bradscheppsg@hotmail.com>
2024-05-09 13:37:28 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
+const gnomeSort = (arr) => {
+  let index = 0;
+  while (index < arr.length) {
+    if (index === 0 || arr[index] >= arr[index - 1]) {
+      index++;
+    } else {
+      [arr[index], arr[index - 1]] = [arr[index - 1], arr[index]];
+      index--;
+    }
+  }
+  return arr;
+};
