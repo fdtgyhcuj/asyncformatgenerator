6a7757375e48d6f8bfb3304689750ea6e5cc46cf
fdtgyhcuj <bradscheppsg@hotmail.com>
2024-05-16 14:16:09 +0700

    update task

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const person = { name: "Alice", age: 30 };
-if (person.hasOwnProperty("age")) {
-  console.log("The person has an age property");
+function lengthOfLIS(nums) {
+  if (nums.length === 0) return 0;
+  const dp = new Array(nums.length).fill(1);
+  let max = 1;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+    max = Math.max(max, dp[i]);
+  }
+  return max;
 }
